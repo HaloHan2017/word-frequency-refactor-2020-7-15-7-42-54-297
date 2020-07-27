297117ddcee190115f9a2f78fe0ee4dd59fa09da
HaloHan2017 <2332722908@qq.com>
2020-07-27 17:03:19 +0800

    [jerry] extract calculateWordFrequency method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -10,27 +10,22 @@ public class WordFrequencyGame {
             return sentence + " 1";
         } else {
             try {
-                String[] words = sentence.split(SPACE_PATTERN);
-                List<WordInfo> wordInfos = new ArrayList<>();
-                for (String word : words) {
-                    WordInfo wordInfo = new WordInfo(word, 1);
-                    wordInfos.add(wordInfo);
-                }
-                Map<String, List<WordInfo>> map = getListMap(wordInfos);
-                List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
-                    WordInfo input = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                wordInfos = list;
-                wordInfos.sort((firstWordInfo, secondWordInfo) -> firstWordInfo.getWordCount() - secondWordInfo.getWordCount());
+                List<WordInfo> wordInfos = calculateWordFrequency(sentence);
+                wordInfos.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
                 return generateWordFrequencyResult(wordInfos);
             } catch (Exception e) {
                 return CALCULATE_ERROR;
             }
         }
     }
-
+    private List<WordInfo> calculateWordFrequency(String sentence){
+        List<WordInfo> wordInfos = new ArrayList<>();
+        List<String> words = Arrays.asList(sentence.split(SPACE_PATTERN));
+        for (String uniqueWord : new HashSet<>(words)){
+            wordInfos.add(new WordInfo(uniqueWord , Collections.frequency(words , uniqueWord)));
+        }
+        return wordInfos;
+    }
     private String generateWordFrequencyResult(List<WordInfo> wordInfos) {
         StringJoiner joiner = new StringJoiner(DELIMITER);
         for (WordInfo wordInfo : wordInfos) {
